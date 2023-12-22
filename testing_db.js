210bcdb17a1120ef5f342161bb276af113ba924a
Dino <61086008+Dinomaster818@users.noreply.github.com>
2023-12-22 16:24:05 +0100

    Update testing_db.js

--- testing_db.js
+++ testing_db.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 
-const dbPath = path.join(__dirname, 'usersdb');
+const dbPath = path.join(__dirname, 'usersdb.db');
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
