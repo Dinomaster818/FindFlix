eb30e5d579c78374b173a0181adf94cd28e4f5f9
Ellyas Rahimy <ellyasrahimy@gmail.com>
2024-01-06 18:59:57 +0100

    Login
    har fixat login och registration page. Det enda som är kvar är att fixa så att sign in och register knapparna funkar. Har försökt med den men har inte hittat en lösning

---
+++ views/login.js
@@ -0,0 +1,27 @@
+function signinbtn() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var db = new sqlite3.Database(dbPath, (err) => {
+        if (err) {
+            console.error('Error opening database:', err.message);
+        } else {
+            console.log('Connected to the database');
+        }
+    });
+}
+
+function registerbtn () {
+    
+    var first_name = document.getElementById("first_name").value;
+    var last_name = document.getElementById("last_name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var db = new sqlite3.Database(dbPath, (err) => {
+        if (err) {
+            console.error('Error opening database:', err.message);
+        } else {
+            console.log('Connected to the database');
+        }
+    });
+}
+
