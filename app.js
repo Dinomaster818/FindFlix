269c4297d6715d19cc7082424772ccfe136bc078
Daniel Moradi <Daniel.moradi15@gmail.com>
2024-03-28 22:37:52 +0100

    The main projekt

--- app.js
+++ app.js
@@ -9,34 +9,37 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
-});
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Define the root route to serve the 'index.html' file
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use(function (err, req, res, next) {
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Send the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.sendFile(path.join(__dirname, 'views/error.html'));
 });
 
 module.exports = app;
