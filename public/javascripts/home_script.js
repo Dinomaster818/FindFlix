3bee946022dabb0cc5c5b7693177be814082a1a1
Daniel Moradi <Daniel.moradi15@gmail.com>
2024-04-01 13:09:02 +0200

    add-watchlist

--- public/javascripts/home_script.js
+++ public/javascripts/home_script.js
@@ -27,14 +27,14 @@ var swiper = new Swiper(".mySwiper", {
 
 // Check if the clicked button is already active
 function changeBackgroundColor(event) {
-    if (event.target.style.backgroundColor === 'red') {
+    if (event.target.style.backgroundColor === 'blue') {
         event.target.style.backgroundColor = '';
     } else {
         var buttons = document.querySelectorAll('.category-btn');
         buttons.forEach(function (button) {
             button.style.backgroundColor = '';
         });
-        event.target.style.backgroundColor = 'red';
+        event.target.style.backgroundColor = 'blue';
     }
 }
 
